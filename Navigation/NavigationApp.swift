1b454dbea0053b86193c5f24e169f98ec2edecc0
philip713 <105677806+philip713@users.noreply.github.com>
2023-11-08 00:47:06 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Philip Janzel Paradeza on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
